b94676cf005ea4a7795623a313c72ba418d43326
Jordan <jdkauffman11@gmail.com>
2016-09-09 16:39:35 -0500

    Added firebase to the project

---
+++ js/Firebase.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jordankauffman on 9/9/16.
+ */
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCSJE-459z2nnG6fvXFuoFvBb4Wq4KgpeQ",
+    authDomain: "student-recognition-se329.firebaseapp.com",
+    databaseURL: "https://student-recognition-se329.firebaseio.com",
+    storageBucket: "student-recognition-se329.appspot.com",
+};
+firebase.initializeApp(config);
+
+
